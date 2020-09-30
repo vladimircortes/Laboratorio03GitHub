c8fbb5b2d9f8b4c2ee1f25afd462f35a5fec7277
Vladimir Cortés A <vladimir.cortes@misena.edu.co>
2020-09-30 12:41:32 -0500

    Subimos el programa completo y 100% Terminado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio03GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /* Solicito el nombre del usuario*/
+            Console.WriteLine("\nWhat is your name? ");
+            /* Capturo los datos (Nombre y  fecha) en unas variables */
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            /* Imprimir los resultados por Pantalla */
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
